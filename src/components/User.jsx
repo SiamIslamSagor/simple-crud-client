59fd37a04407a6c28224cd100dce9cb4f2b636b8
SiamIslamSagor <msiamislam12345@gmail.com>
2023-10-14 01:59:08 +0600

    delet user in database

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -2,14 +2,32 @@ import { useLoaderData } from "react-router-dom";
 
 const User = () => {
   const users = useLoaderData();
+  console.log(users);
+
+  const handleDelete = id => {
+    console.log(id);
+    //
+    fetch(`http://localhost:5000/users/${id}`, {
+      method: "DELETE",
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        if (data.deletedCount > 0) {
+          alert("Deleted Successfully");
+        }
+      });
+  };
 
   return (
     <div>
+      <h1>fg</h1>
       <h2>Users: {users.length}</h2>
       <div>
         {users.map(user => (
           <p key={user._id}>
-            {user.name} : {user.email}
+            {user.name} : {user.email}{" "}
+            <button onClick={() => handleDelete(user._id)}>X</button>
           </p>
         ))}
       </div>
